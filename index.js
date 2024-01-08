a785a0bb3d6e7e02abdebbdc2fb604815e76fa94
swatiswagatika98 <swatiswagatika.1998mishra@gmail.com>
2024-01-08 09:20:07 -0800

    getElementsByTagName

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-let parent=document.getElementById('header');
-let h2=document.createElement('h3');
-let text=document.createTextNode('Buy high quality organic fruits online');
-h2.appendChild(text);
-h2.style.fontStyle="italic";
-parent.appendChild(h2)
-let fruit=document.getElementById('basket-heading');
-let para=document.createElement('p');
-para.setAttribute('id','fruits-total')
-let txt=document.createTextNode('Total fruits:4')
-para.appendChild(txt)
-fruit.appendChild(para);
-console.log(fruit)
+const lis = document.getElementsByTagName("li");
+const mangoLi = lis[4];
+mangoLi.style.color = "blue";
+for(const Li of lis){
+    console.log(Li)
+    Li.style.fontStyle = "italic"
+}
